175ae4cd4a1219c7a9cca6d9e2278a627048f6dc
yfedyna <yfedyna@griddynamics.com>
2023-03-02 13:27:02 +0200

    start

---
+++ src/main/java/com/yfedyna/Dishes/DishesApplication.java
@@ -0,0 +1,13 @@
+package com.yfedyna.Dishes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DishesApplication.class, args);
+	}
+
+}
